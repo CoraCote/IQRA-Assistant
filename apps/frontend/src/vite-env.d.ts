e6ddeeca4a2b43be2f3f1ca8cf11811a685554f2
CoraCote <cora.cote0903@gmail.com>
2025-09-17 07:12:15 -0400

    vite-env file add

---
+++ apps/frontend/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL?: string
+  readonly VITE_PUBLIC_AGENT_ID?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+
